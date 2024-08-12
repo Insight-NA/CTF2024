be78bd2e9e3d3def423eb34d8ab2fd3c7bf9aa41
Andrew <andrew.sutliff@insight.com>
2024-08-12 09:22:56 -0400

    changes to infra .sh

--- Kubernetes-Cypto-Miner/infrastructure.sh
+++ Kubernetes-Cypto-Miner/infrastructure.sh
@@ -1,3 +1,7 @@
-az aks create -n ctfCluster -g transform2023-rg  --generate-ssh-keys --attach-acr ctfContainerRegistry
+az group create -n MasteryCTF_Andrew -l eastus
 
-az aks get-credentials -n ctfCluster -g transform2023-rg
+az acr create -n ctfcontainerregistry -g MasteryCTF_Andrew --sku Basic
+
+az aks create -n ctfCluster -g MasteryCTF_Andrew --generate-ssh-keys --attach-acr ctfcontainerregistry
+
+az aks get-credentials -n ctfCluster -g MasteryCTF_Andrew 
